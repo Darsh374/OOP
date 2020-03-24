f126193e1c46171a44544d86e35f545437597240
Darsh374 <dp594@njit.edu>
2020-03-24 11:00:21 -0400

    task: added subtraction function

--- Operations/addition.py
+++ Operations/addition.py
@@ -2,4 +2,3 @@ class Addition:
     @staticmethod
     def sum(augend, addend):
         return augend + addend
-    
---
+++ Operations/subtraction.py
@@ -0,0 +1,6 @@
+class Subtraction:
+    @staticmethod
+    def difference(minuend, subtraend):
+        return minuend - subtraend
+
+        
